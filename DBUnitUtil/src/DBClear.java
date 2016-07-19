4dea686b3d7027dde6e80542aa2cf0cc4b2a7e96
kresslib <kresslib@gmail.com>
2016-07-19 14:43:51 +0300

    Upload

---
+++ DBUnitUtil/src/DBClear.java
@@ -0,0 +1,23 @@
+
+import java.sql.Connection;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author ikress
+ */
+public class DBClear {
+
+    public static void tablesDrop(Connection con, String[] tables) {
+
+    }
+
+    public static String[] getTables(String createAllTablesScript) {
+
+        return new String[]{};
+    }
+}
